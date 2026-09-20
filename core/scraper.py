96e92e9f44798ef825830be55292e74599ef4317
NARARYA JAYA UTAMA GROUB <nararyajayautamagroub@gmail.com>
2026-09-20 15:36:06 +0700

    fix: remove unused scraper urlopen import

--- core/scraper.py
+++ core/scraper.py
@@ -11,7 +11,6 @@ from urllib.request import (
     HTTPRedirectHandler,
     Request,
     build_opener,
-    urlopen,
 )
 from urllib.robotparser import RobotFileParser
 from datetime import datetime, timezone
